b4e85355c89335b3ceba90030b5ed074e2107074
PawZaw <49035966+PawZawDev@users.noreply.github.com>
2019-11-23 03:32:19 +0100

    kind of works but not really

--- Task4cFront/screens/MainScreen.js
+++ Task4cFront/screens/MainScreen.js
@@ -1,31 +1,45 @@
 import React, { useState, Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ScrollView, TouchableOpacity } from 'react-native';
 
-function MainScreen() {
+function MainScreen(props) {
   const [outputText, setOutputText] = useState("It's a new world");
-    return (
-      <View style={styles.everything}>
-        <View style={styles.topView}>
-          <Text>{outputText}</Text>
-          <Text>{outputText}</Text>
-          <Text>{outputText}</Text>
-          <Text>{outputText}</Text>
-          <Button title="it's a new life" onPress={() => setOutputText("It's a new day")} />
+  return (
+    <View style={styles.everything}>
+      <ScrollView style={styles.singleRowScrollView}>
+        <View style={styles.singleRowView}>
+          <TouchableOpacity onPress={() => props.navigation.goBack()}>
+            <Image style={styles.singleActivity} source={require("../assets/images/morda.jpg")} />
+          </TouchableOpacity>
+            <Image style={styles.singleActivity} source={require("../assets/images/morda.jpg")} />
         </View>
-      </View>
-    );
+        <Text>{outputText}</Text>
+        <Text>{outputText}</Text>
+      </ScrollView>
+      <Text>dddupaaaaaaaaaaaaaaaaaaaaaaaaaaaa</Text>
+      <Button title="it's a new life" onPress={() => setOutputText("It's a new day")} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   everything: {
-    padding: 50,
-    flexDirection: 'row',
-  },
-  topView: {
-    flex: 1,
     backgroundColor: '#ccc',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 50,
+    flexDirection: 'column',
+  },
+  singleRowScrollView: {
+    flexGrow: 1,
+  },
+  singleRowView: {
+    flexDirection: 'row',
+    justifyContent: "space-between",
+  },
+  singleActivity: {
+    flex: 1,
+    width: 70,
+    height: 70, 
   },
 });
 
